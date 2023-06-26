0f0c433340189828588fb3f07a30dff06816b9d9
WhopMe <79413124+WhopMe@users.noreply.github.com>
2023-06-26 16:28:35 +0530

    Update Home.svelte

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -3,7 +3,7 @@
 		class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
 	>
 		Get curated show <br class="hidden md:block" /> and movie recommendations
-		<br class="hidden md:block" /> with Open AI
+		<br class="hidden md:block" /> with DogAI
 	</div>
 	<div class="flex flex-wrap items-center mr-auto">
 		<button
@@ -13,11 +13,11 @@
 			Get recommendations
 		</button>
 		<a
-			href="https://www.youtube.com/watch?v=Z3LrfdctOpU"
+			href="https://www.dogai.com/"
 			target="_blank"
 			class="w-full md:w-auto text-center bg-white/20 hover:bg-white/50 mb-4 flex-none rounded-full text-white font-medium py-3 px-6"
 		>
-			Learn how it's built
+			Go back to Homepage
 		</a>
 	</div>
 </div>
